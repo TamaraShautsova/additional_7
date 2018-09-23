9883cf8883bf2a6ebbc0b4a939cafb156f142d5a
tamara shautsova <t.shautsova@gmail.com>
2018-09-23 17:49:22 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for (var row = 0; row < 9; row++) {
+    for (var col = 0; col < 9; col++) {
+      if (matrix[row][col] === 0) {
+        var array = [];
+
+        //little square
+        for (var i = 0; i < 9; i ++) {
+          array.push(matrix[i][col]);
+          array.push(matrix[row][i]);
+          var smallRow = Math.floor(row / 3) * 3;
+          var smallCol = Math.floor(col / 3) * 3;
+          var squareNumbers = matrix[smallRow + i % 3][smallCol + Math.floor(i / 3)];
+          array.push(squareNumbers);
+        }
+
+
+        var excessNumber = [];
+        var all = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for (var c = 0; c < all.length; c++) {
+          if (array.indexOf(all[c]) < 0) {
+            excessNumber.push(all[c]); 
+          }
+        }
+
+        for (var d = 0; d < excessNumber.length; d++) {
+          matrix[row][col] = excessNumber[d];
+          var solved = solveSudoku(matrix);
+          if (solved !== false) {
+          return solved;
+          }
+        }
+        matrix[row][col] = 0;
+        return false; 
+      }
+    }
+  }
+  return matrix;
 }
